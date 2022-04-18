95475b2491ac0ef6c26b74be8165a2f5263211e9
Андрей Кузьменко <triplefork88@gmail.com>
2022-04-18 15:21:00 +0300

    1st commit

---
+++ src/main/java/com/software/triviabot/TriviabotApplication.java
@@ -0,0 +1,13 @@
+package com.software.triviabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviabotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/software/triviabot/TriviabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.triviabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
