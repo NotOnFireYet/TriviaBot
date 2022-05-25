1c19671c780f1cf9d0b2963a8448ed42cdf63c4f
Андрей Кузьменко <triplefork88@gmail.com>
2022-05-25 18:38:14 +0300

    refactored message method variables

--- src/main/java/com/software/triviabot/cache/ActiveMessageCache.java
+++ src/main/java/com/software/triviabot/cache/ActiveMessageCache.java
@@ -1,38 +1,51 @@
 package com.software.triviabot.cache;
 
+import com.software.triviabot.model.UserCache;
+import com.software.triviabot.repo.object.UserCacheRepo;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.objects.Message;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class ActiveMessageCache {
     // message to be edited during quiz.
     // whole quiz consists of refreshing this message
-    private static Map<Long, Message> refreshMessageMap = new HashMap<>();
-
+    private static Map<Long, Integer> refreshMessageMap = new HashMap<>();
     // message to be deleted
-    private static Map<Long, Message> deleteMessageMap = new HashMap<>();
+    private static Map<Long, Integer> deleteMessageMap = new HashMap<>();
 
-    private ActiveMessageCache(){}
+    @Autowired
+    private static UserCacheRepo cacheRepo; // todo: stopped on this throwing a nullpointer. there may be a way to have a custom shutdown hook.
 
-    public static void setRefreshMessage(long userId, Message message){
-        refreshMessageMap.put(userId, message);
+    public static void setRefreshMessageId(long userId, int messageId){
+        /*UserCache cache = cacheRepo.findByUserId(userId);
+        cache.setRefreshMessageId(messageId);
+        cacheRepo.saveCache(cache);*/
+
+        refreshMessageMap.put(userId, messageId);
     }
 
     public static int getRefreshMessageId(long userId){
-        return refreshMessageMap.get(userId).getMessageId();
+        return refreshMessageMap.get(userId);
     }
 
-    public static Message getDeleteMessage(long userId){
+    public static int getDeleteMessageId(long userId){
         return deleteMessageMap.get(userId);
     }
 
-    public static void setDeleteMessage(long userId, Message message) {
-        deleteMessageMap.put(userId, message);
+    public static void setDeleteMessageId(long userId, int messageId) {
+        /*UserCache cache = cacheRepo.findByUserId(userId);
+        cache.setDeleteMessageId(messageId);
+        cacheRepo.saveCache(cache);*/
+
+        deleteMessageMap.put(userId, messageId);
     }
 
     public static void clearCache(long userId) {
